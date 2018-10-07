b08acfcee8e1f742a897961507df408fe285adc2
zgjstudy <981225888@qq.com>
2018-10-07 17:03:19 +0800

    BSTNode finished

--- BinaryTree/BSTNode.h
+++ BinaryTree/BSTNode.h
@@ -8,9 +8,46 @@
 
 
 #pragma once
+#include "BinNode.h"
 
-template <typename key, typename E>
+template <typename Key, typename E>
 class BSTNode : public BinNode<E>
 {
+private:
+	Key k;
+	E it;
+	BSTNode* lc;	//left child
+	BSTNode* rc;
 
+public:
+	BSTNode()
+	{
+		lc = rc = nullptr;
+	}
+
+	BSTNode(Key K, E e, BSTNode* l = nullptr, BSTNode* r = nullptr)
+	{
+		k = K;
+		it = e;
+		lc = l;
+		rc = r;
+	}
+
+	~BSTNode() {};
+
+	E& element() { return it; }
+	void setElement(const E& e) { it = e; }
+
+	Key& key() { return k; }
+	void setKey(const Key& K) { k = K; }
+
+	inline BSTNode* left() const { return lc; }
+	void setLeft(BinNode<E>* b) { lc = (BSTNode*)b; }
+	inline BSTNode* right() const { return rc; }
+	void setRight(BinNode<E>* b) { rc = (BSTNode*)b; }
+
+	bool isLeaf()
+	{
+		return (lc == nullptr) && (rc = nullptr);
+	}
 };
